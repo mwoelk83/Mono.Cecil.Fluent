7a100411dfa61247d087f02a2a6bacf3dd06dd29
Marcus Wölk <mwoelk83@gmail.com>
2015-11-12 17:00:02 +0100

    Extension MethodDefinition.Returns(..) added

---
+++ Mono.Cecil.Fluent.Tests/Extensions/MethodDefinition.Returns.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.IO;
+using Machine.Specifications;
+// ReSharper disable All
+
+namespace Mono.Cecil.Fluent.Tests.Extensions
+{
+	public class Extensions_MethodDefinition_Returns : TestsBase
+	{
+		static TypeDefinition TestType = CreateType();
+		static MethodDefinition TestMethod = TestType.NewMethod();
+
+		It should_return_void =
+			() => TestMethod.ReturnsVoid().ReturnType.ShouldEqual(TestModule.TypeSystem.Void);
+
+		It should_return_typereference =
+			() => TestMethod.Returns(TestType).ReturnType.ShouldEqual(TestType);
+
+		It should_return_system_type =
+			() => TestMethod.Returns(typeof(ArrayList)).ReturnType.FullName.ShouldEqual(typeof(ArrayList).FullName);
+
+		It should_return_system_type_generic =
+			() => TestMethod.Returns<FileInfo>().ReturnType.FullName.ShouldEqual(typeof(FileInfo).FullName);
+	}
+}
---
+++ Mono.Cecil.Fluent/CecilExtensions/MethodDefinition.Returns.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Mono.Cecil.Fluent
+{
+	public static partial class MethodDefinitionExtensions
+	{
+		public static MethodDefinition ReturnsVoid(this MethodDefinition method)
+		{
+			method.ReturnType = method.Module.TypeSystem.Void;
+			return method;
+		}
+
+		public static MethodDefinition Returns(this MethodDefinition method, TypeReference type)
+		{
+			method.ReturnType = method.Module.SafeImport(type);
+			return method;
+		}
+
+		public static MethodDefinition Returns(this MethodDefinition method, Type type)
+		{
+			method.ReturnType = method.Module.SafeImport(type);
+			return method;
+		}
+
+		public static MethodDefinition Returns<T>(this MethodDefinition method)
+		{
+			method.ReturnType = method.Module.SafeImport(typeof(T));
+			return method;
+		}
+	}
+}
